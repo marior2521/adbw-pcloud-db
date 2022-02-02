29021711a70cb2fdaf9d79e1c152ee4744286344
marior2521 <74110086+marior2521@users.noreply.github.com>
2022-02-01 19:09:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-	<title>pCloud</title>
-	<meta name="robots" content="noindex">
-	<link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon" />
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
-	<script src="assets/js/scripts.min.js"></script>
-	<script type="text/javascript" src="https://ssl.p.jwpcdn.com/player/v/8.8.6/jwplayer.js"></script>
-	<script type="text/javascript">jwplayer.key="cLGMn8T20tGvW+0eXPhq4NNmLB57TrscPjd1IyJF84o=";</script>
-	<style type="text/css" media="screen">html,body{padding:0;margin:0;height:100%}#apicodes-player{width:100%!important;height:100%!important;overflow:hidden;background-color:#000}</style>
+				<meta charset="UTF-8">
+				<title>Goku Home</title>
+				<style>
+								.welcome {
+							clear: both;      
+							text-align: center;
+						  color: #2bff82;
+								}
+								
+							.congralutations {
+												color: #ff001f;
+												clear: both;
+												text-align: center;
+								}
+								
+							.developer	{
+												
+								}
+				</style>
 </head>
 <body>
-
-<?php 
-		error_reporting(0);
-		
-		$link = (isset($_GET['link'])) ? $_GET['link'] : '';
-
-		$sub = (isset($_GET['sub'])) ? $_GET['sub'] : '';
-
-		$poster = (isset($_GET['poster'])) ? $_GET['poster'] : '';
-
-		if ($link != '') {
- 			
- 				preg_match('/code=(.*)/', $link, $matchID);
-				echo '<div id="apicodes-player"></div><script type="text/javascript">var shareId = "'.$matchID[1].'", sub = "'.$sub.'", poster = "'.$poster.'"</script>';
-
-		} else echo 'Link not found!';
-?>
+				<h1 class="welcome">Welcome</h1>
+				<h2 class="congralutations">Congralutations you get here.</h2>
+				<h2>API</h2>
+				<h3 class="debeloper">Hi, If you are a developer and you want to use my player api. It can be with the links so that you can use the series, movies, ovas and especials of dragon ball to use on your website, all in different languages, just contact me at marior00876@gmail.com | This is a bad traslation I will try to fixed this later. |</h3>
 </body>
 </html>
-
