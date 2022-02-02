c3f32a86ae902aca2c205ddaf4dba360d0afa7f4
marior2521 <74110086+marior2521@users.noreply.github.com>
2022-02-01 19:09:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -28,3 +28,4 @@
 				<h3 class="debeloper">Hi, If you are a developer and you want to use my player api. It can be with the links so that you can use the series, movies, ovas and especials of dragon ball to use on your website, all in different languages, just contact me at marior00876@gmail.com | This is a bad traslation I will try to fixed this later. |</h3>
 </body>
 </html>
+ 
